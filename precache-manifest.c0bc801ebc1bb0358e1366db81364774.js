cf6bd3c5d53d2808d5528153b424288a2593ebf1
Andres <hernandez.andres.d@gmail.com>
2019-12-17 17:37:12 +1100

    Updates

---
+++ precache-manifest.c0bc801ebc1bb0358e1366db81364774.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8aa48a9e6696ad32566bb4664788c0",
+    "url": "/api-todo-list/index.html"
+  },
+  {
+    "revision": "ed9e763505ab5c595927",
+    "url": "/api-todo-list/static/css/main.cf34733c.chunk.css"
+  },
+  {
+    "revision": "a7818875849d2ed0e3a1",
+    "url": "/api-todo-list/static/js/2.d647fa13.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/api-todo-list/static/js/2.d647fa13.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed9e763505ab5c595927",
+    "url": "/api-todo-list/static/js/main.d14b2555.chunk.js"
+  },
+  {
+    "revision": "6a966fb11b4ddade27e8",
+    "url": "/api-todo-list/static/js/runtime-main.099ccfbc.js"
+  }
+]);
