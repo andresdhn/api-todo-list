ff1d490b7ad2cea97e3a8ff370280b9ef40658c4
Andres <hernandez.andres.d@gmail.com>
2020-02-24 16:59:11 +1100

    Rewrite App with React Hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,38 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import List from './List'
 //
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            todos: [],
-            completed: [],
-        }
-    }
+function App() {
+    const [todos, setTodos] = useState([])
+    const [completed, setCompleted] = useState([])
 
-    updateList = data => {
-        let completed = data.filter(todo => todo.completed !== true)
+    const updateList = data => {
         let todos = data.filter(todo => todo.completed === true)
+        let completed = data.filter(todo => todo.completed !== true)
 
-        this.setState({ todos: todos, completed: completed })
+        setCompleted(completed)
+        setTodos(todos)
     }
 
-    componentDidMount = () => {
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/todos?_page=1&_limit=10')
             .then(res => res.json())
-            .then(json => this.updateList(json))
-    }
+            .then(json => updateList(json))
+    })
 
-    render() {
-        return (
-            <div id="app" className="mt-5">
-                <div className="container">
-                    <div className="row justify-content-md-center">
-                        <div className="col col-md-6">
-                            <List title="Todo" items={this.state.todos} />
-                        </div>
-                        <div className="col col-md-6">
-                            <List
-                                title="Completed"
-                                items={this.state.completed}
-                            />
-                        </div>
+    return (
+        <div id="app" className="mt-5">
+            <div className="container">
+                <div className="row justify-content-md-center">
+                    <div className="col col-md-6">
+                        <List title="Todo" items={todos} />
+                    </div>
+                    <div className="col col-md-6">
+                        <List title="Completed" items={completed} />
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default App
